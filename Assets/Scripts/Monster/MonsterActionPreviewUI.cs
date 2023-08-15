3e0667777dd911c95dcc8274f8bbdc57d3c460f9
Martin Zackrisson <martin.zackrisson@fripost.org>
2023-08-15 16:57:33 +0200

    Improve previews a bit

--- Assets/Scripts/Monster/MonsterActionPreviewUI.cs
+++ Assets/Scripts/Monster/MonsterActionPreviewUI.cs
@@ -6,38 +6,20 @@ using UnityEngine;
 public class MonsterActionPreviewUI : MonoBehaviour
 {
     [SerializeField]
-    TMPro.TextMeshProUGUI CooldownText;
-
-    [SerializeField]
-    GameObject CooldownCover;
-
-    [SerializeField]
-    Image Preview;
+    Image IconImage;
 
     [SerializeField]
     TooltipedItem Tooltip;
 
     [SerializeField]
-    TMPro.TextMeshProUGUI ValueText;
+    TMPro.TextMeshProUGUI CooldownText;
 
 
     public void Sync(MonsterAction action, int remainingAP)
     {
-        Preview.sprite = action.Sprite;
-        ValueText.text = action.Value.ToString();
+        IconImage.sprite = Iconography.GetAction(action.ActionType);
+        CooldownText.text = action.Cooldown.ToString();
 
         Tooltip.SetTooltip(action.Name);
-
-
-        if (!action.IsOnCooldown && action.ActionPoints <= remainingAP)
-        {
-            CooldownCover.SetActive(false);
-            return;
-        }
-
-        CooldownCover.SetActive(true);
-        var cooldown = action.Cooldown;
-
-        CooldownText.text = cooldown == 0 ? "" : cooldown.ToString();
     }
 }
